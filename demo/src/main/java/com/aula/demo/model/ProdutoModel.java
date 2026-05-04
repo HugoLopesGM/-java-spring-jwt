29049ccf71a1abf7ef703bbf0b3467b2a6b8c497
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-05-04 19:30:38 -0300

    ajustando erro

--- demo/src/main/java/com/aula/demo/model/ProdutoModel.java
+++ demo/src/main/java/com/aula/demo/model/ProdutoModel.java
@@ -1,4 +1,42 @@
 package com.aula.demo.model;
 
+import jakarta.persistence.*;
+@Entity
+@Table
 public class ProdutoModel {
-}
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String nome;
+        private Integer quantidade;
+
+
+        public  ProdutoModel(){
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getQuantidade() {
+            return quantidade;
+        }
+
+        public void setQuantidade(Integer quantidade) {
+            this.quantidade = quantidade;
+        }
+    }
+
